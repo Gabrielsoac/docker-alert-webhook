7895fe3a1b252a08ec3d1503c6ccaffa89681968
Gabriel Soares <gabrielsoacc@gmail.com>
2024-10-03 16:00:37 -0300

    feat: created integration with webhook for docker events alert

--- events.py
+++ events.py
@@ -1,11 +1,13 @@
 import docker as dkr
 import datetime as dt
+import requests as rq 
 
 ##client = dkr.from_env() -> busca pelas variáveis de ambiente
 
 client = dkr.DockerClient(base_url='unix://var/run/docker.sock')
+webhook_discord_url = input("Enter the webhook_url: ")
 
-for event in client.events(decode=True, filters={'event':'die'}):
+for event in client.events(decode=True):
     #if(event['Action'] == 'die'):
         #print(event)
 
@@ -13,8 +15,10 @@ for event in client.events(decode=True, filters={'event':'die'}):
     container_name = event["Actor"]["Attributes"]["name"]
     container_epoch_time = event["time"]
     container_time_stamp = dt.datetime.fromtimestamp(container_epoch_time)
-    print(f"Alert \n Container ID: {container_id}\n Container Name: {container_name}\n Time: {container_time_stamp}")
-
+    
+    payload = {"content": f"Alert \n Container ID: {container_id}\n Container Name: {container_name}\n Time: {container_time_stamp}"}
+    rq.post(webhook_discord_url, data=payload)  
+    
 client.close()
 
 
