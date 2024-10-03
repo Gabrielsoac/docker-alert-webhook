e4effc6d206b9fdeae0af7504f12dfda862a21d5
Gabriel Soares <gabrielsoacc@gmail.com>
2024-10-03 15:17:40 -0300

    feat: converted epoch time for time stamp

--- events.py
+++ events.py
@@ -1,4 +1,5 @@
 import docker as dkr
+import datetime as dt
 
 ##client = dkr.from_env() -> busca pelas variáveis de ambiente
 
@@ -11,8 +12,8 @@ for event in client.events(decode=True, filters={'event':'die'}):
     container_id = event["id"]
     container_name = event["Actor"]["Attributes"]["name"]
     container_epoch_time = event["time"]
-    
-    print(f"Alert \n Container ID: {container_id}\n Container Name: {container_name}\n Time: {container_epoch_time}")
+    container_time_stamp = dt.datetime.fromtimestamp(container_epoch_time)
+    print(f"Alert \n Container ID: {container_id}\n Container Name: {container_name}\n Time: {container_time_stamp}")
 
 client.close()
 
